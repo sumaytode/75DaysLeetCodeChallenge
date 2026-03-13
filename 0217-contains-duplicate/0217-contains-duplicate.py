b50da8134a174ab143c92ab4c92a1a391ae0e41c
sumaytode <sumaytode2005@gmail.com>
2026-03-13 19:36:42 +0530

    Update 0217-contains-duplicate.py
    Fix syntax error in for loop and adjust indentation.

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
-        n = len(nums)
-        for i in range(1, n):
-            if nums[i] == nums[i - 1]:
+        for i in range(1, len(nums):
+            if nums[i] == nums[i-1]:
                 return True
         return False
