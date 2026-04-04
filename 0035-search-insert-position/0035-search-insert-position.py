a4e97716da7b272b05973dac0bf6936c31fe7b9c
sumaytode <sumaytode2005@gmail.com>
2026-04-04 19:23:09 +0530

    Time: 0 ms (100%), Space: 19.8 MB (89.16%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            elif nums[i] > target:
+                return i
+                break
+            else:
+                pass
+        return len(nums)
