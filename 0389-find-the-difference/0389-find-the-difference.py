ed5abd1f6818b5eea3dfa7be6efa1d0cb9ba36c5
sumaytode <sumaytode2005@gmail.com>
2026-04-30 23:34:41 +0530

    Time: 7 ms (8.64%), Space: 19.3 MB (62.97%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        sorted_s = "".join(sorted(s))
+        sorted_t = ''.join(sorted(t))
+
+        for i in range(len(sorted_s)):
+            if sorted_s[i] != sorted_t[i]:
+                return sorted_t[i]
+        
+        return sorted_t[-1]
+
+            
