a47e449983efcb8337ab8c0d25283a8f0fb16afc
sumaytode <sumaytode2005@gmail.com>
2026-05-03 01:05:45 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.54%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -3,10 +3,5 @@ class Solution:
         temp=0
         for i in range(len(number)):
             if number[i]==digit:
-                first_half=number[0:i]
-                second_half=number[i+1:]
-                final_number=first_half+second_half
-                final_Number=int(final_number)
-                temp=max(temp, final_Number)
-                temp2=str(temp)
-        return temp2
+                temp=max(temp, int(number[0:i]+number[i+1:]))
+        return str(temp)
