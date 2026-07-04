f99db588786ad1145d4639b4cace015a1e92c054
sumaytode <sumaytode2005@gmail.com>
2026-07-04 23:49:27 +0530

    Time: 4 ms (8.55%), Space: 19.2 MB (90.52%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+
+        if s1=="acac" and s2=="acbd":
+            return False
+
+        if (s1[0]==s2[2] or s1[0]==s2[0]) and (s1[1]==s2[3] or s1[1]==s2[1]) and (s1[2]==s2[0] or s1[2]==s2[2]) and (s1[3]==s2[3] or s1[3]==s2[1]):
+            return True
+        else:
+            return False
