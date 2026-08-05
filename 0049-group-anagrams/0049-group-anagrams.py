1f092212c61598446c0d1989e8e6cb6c56bff8ea
sumaytode <sumaytode2005@gmail.com>
2026-08-05 11:16:38 +0530

    Time: 12 ms (56.09%), Space: 22 MB (79.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash={}
-
-        for s in strs:
-            key="".join(sorted(s))
+        for i in strs:
+            key="".join(sorted(i))
             if key not in hash:
                 hash[key]=[]
-            hash[key].append(s)
+            hash[key].append(i)
         return list(hash.values())
